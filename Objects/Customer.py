4cf47422b03f769f0e0dea47a3ab4180fe191131
narekatturohan-sketch <narekattu.rohan@gmail.com>
2026-04-13 22:41:05 +0530

    Customers

---
+++ Objects/Customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    def __init__(self, Title, FirstName, LastName, Street, City, State, ZipCode):
+        self.Title = Title
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Street = Street
+        self.City = City
+        self.State = State
+        self.ZipCode = ZipCode
